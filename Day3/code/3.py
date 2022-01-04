3d2b5ac08f50c932e75ce4855f40a7d1314e1e93
letzzBuild <letzzBuild@gmail.com>
2022-01-04 20:37:22 +0530

    day 3 added

---
+++ Day3/code/3.py
@@ -0,0 +1,64 @@
+# print("Hello world")
+
+
+# this function name is Hello and it prints letzzBuild 5 times on the terminal
+# def Hello():
+#     print("letzzbuild")
+#     print("letzzbuild")
+#     print("letzzbuild")
+#     print("letzzbuild")
+#     print("letzzbuild")
+
+# Hello()
+
+
+
+#datatypes variables
+# store integer value 5 in a varible a
+# a = 5
+# print(type(a))
+
+
+#store and show me 10.6(floating point) value in any variable
+# x = 10.6 
+# print(type(x))
+
+#store a character in a variable
+# y = 'z'
+
+#store a string in a varible ""
+# z = "letzzbuild"
+
+#how to create multiple lines in a comments == DOCSTRING
+"""
+xyz
+zyx
+multiple line
+
+"""
+
+#how to take input in python
+#input function will always return a string
+abc = input("Enter a number : ")
+
+print(abc)
+print(type(abc))
+
+result = int(abc)
+print(result)
+print(type(result))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
