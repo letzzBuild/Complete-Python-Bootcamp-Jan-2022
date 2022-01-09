1c236fe1ebfd148f0031fba5a17525cdd68025f5
letzzBuild <letzzBuild@gmail.com>
2022-01-09 17:23:54 +0530

    day 4 completed

---
+++ Day4/code/day4.py
@@ -0,0 +1,49 @@
+#write a program to print letter z
+#output of program should look like this
+#*****
+#   *
+# * 
+#*****
+
+# print("*****")
+# print("   *")
+# print("  *")
+# print("*")
+# print("*****")
+
+
+
+#write a program which takes name as input and display the lenght of your name
+# name = input("Enter your name : ")
+
+# print(len(name))
+
+#String Concatination 
+# firstname = "Gautam "
+# lastname = "Patil"
+# fullname = firstname + lastname
+# print(fullname)
+
+#converting Datatype
+#write a program to take input age from user and print this line "Your Age is age"
+# age = int(input("Enter your age : "))
+# print(type(age))
+# print("Your Age is " + str(23))
+
+#implict type conversion
+# age = 5.8 +  5  #float + AutoConvertedToFloat = float
+# print(type(age))
+
+
+#print any variable of any datatype inside a string with help of string formating or f strings
+# age = 34
+# print(f"Your age is {age}" )
+# print("Your age is {}".format(age))
+
+
+
+
+
+
+
+
