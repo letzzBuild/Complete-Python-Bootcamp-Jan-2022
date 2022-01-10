c922a1fa8a235b2eac8fc9d835e06008f3095a0f
letzzBuild <letzzBuild@gmail.com>
2022-01-10 20:06:05 +0530

    day 5 updated

---
+++ Day4/assigment-sol.py
@@ -0,0 +1,9 @@
+member1 = input("Enter family member name 1 ")
+age1 = int(input("Enter age of member 1 "))
+member2 = input("Enter family member name 2 ")
+age2 = int(input("Enter age of member 2 "))
+
+totalname = member1 + member2
+
+print("My Total Family Name is {}".format(totalname))
+print(f"My Total Family Age is {age1+age2} ")
---
+++ Day5/Code/Day5.py
@@ -0,0 +1,56 @@
+#syntax of conditional statements
+#if condition:
+# if condition:
+    #do something
+    #all your decision making is done here
+
+#if-else
+# if condition:
+#     #do something
+#     #all your decision making is done here
+# else:
+#     #do something else
+#     #all your decision making is done here
+  
+
+#elif
+# if condition:
+#     #some something
+# elif condition:  
+#     #some other thing
+# else:
+#     #some other thing
+
+#nested if 
+# if condition inside another if condition
+# if condition1:
+#     #some thing
+#     if condition2:
+#         if condition3:
+            #condition 3 code
+
+ 
+
+
+
+
+
+#write a program to take input from user a color and print the vechicle should 
+#1.stop 
+#2.Get ready 
+#3.Go
+#Simulation of Traffice signal
+
+# color = input("Enter a color :")
+# if color == "red" or color == "Red":
+#     print("Stop")
+# elif color == "green":
+#     print("Go") 
+# elif color == "orange":
+#     print("Get Ready")
+# else:
+#     print("Invalid color given")
+
+
+
+
