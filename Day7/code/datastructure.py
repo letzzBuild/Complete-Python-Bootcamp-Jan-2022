791fd493cb426173e932b54240e17bda6e17b737
letzzBuild <letzzBuild@gmail.com>
2022-01-14 19:48:06 +0530

    Day7 added

---
+++ Day7/code/datastructure.py
@@ -0,0 +1,91 @@
+#store 5 numbers in single variable 
+#homogenous values
+# x = [2,3,4,56,7]
+# print(x)
+
+#store heterogeneous data in single variable
+# x = ["Hello", 2 , 4.5 , 'A' , False]
+# print(x)
+
+#Indexing in List
+# x = [2,3,4,56,7]
+#Negative Indices is also possible
+
+
+#Accessing Values of List
+# x = [2,3,4,56,7]
+# access 3rd element of the list
+# print(x[-1])
+
+#Slicing in List
+# x = [2,4,5,7,81,9,34,56,78,90]
+# print(x[:-1])
+
+#List Concatination and List Replication
+# x = [1,2,3]
+# y = [2,3,4]
+# print(x+y)
+
+# x = [1,2,3]
+# print(x*3)
+
+#list are mutable
+# x = [1,2,3,56]
+
+#reassign value of 3rd element of the List
+# x[2] = 5
+
+# print(x)
+
+
+#List Methods/Functions
+# what are some of the built in functions available in a list
+# x = [3,5,6]
+#add a element 8 at the last of the last
+# x.append(8)
+#add element at some specifice index 
+# x.insert(1,76)
+
+#remove eleemnt from the list
+# x.remove(90)
+
+#del keyword is used to delete the element from the list
+# del x[2]
+# print(x)
+
+
+#Tuple
+# x = (2,4,67)
+# x[0] = 5 #not possible
+
+# x = ["Hello",2,4.5,"A",False] #iterablle
+
+# print(len(x))
+
+# for i in listname:
+#     print(i)
+
+# for index,ele in enumerate(x):
+#     print(ele)
+
+
+#while loop
+# x = [1,2,3,4,5,6,7,8,9,10]
+# y = [11,12,13,14,15,16,17,18,19,20]
+# i = 0
+# while(i<len(x)):
+#     #replace the value of x[i] with y[i]
+#     x[i] = y[i]
+#     i+=1
+# print(x) 
+
+#multipy all the elements of the list by 3 
+# x = [1,2,3]
+# for i in x:
+#     print(i*2)
+   
+
+
+
+
+
